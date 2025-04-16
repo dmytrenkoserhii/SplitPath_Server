252816d93e8fcb201f38e6932323076b72ecc667
Serhii <dmtrnk.srg@gmail.com>
2025-04-16 19:11:39 +0300

    feat(DEV-1): set up basic app module, add default shared module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,18 @@
+import * as Joi from 'joi';
+
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+
+import { ENV_VALIDATION } from './shared/constants';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env.development',
+      validationSchema: Joi.object(ENV_VALIDATION),
+    }),
+  ],
   controllers: [],
   providers: [],
 })
