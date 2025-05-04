0020549af232dfdfd41d29facd4f367ad3607a01
Serhii <dmtrnk.srg@gmail.com>
2025-05-04 22:02:38 +0300

    feat(DEV-6): db and entity update for user

--- src/modules/users/entities/user.entity.ts
+++ src/modules/users/entities/user.entity.ts
@@ -41,6 +41,9 @@ export class User {
   @Column({ default: false })
   isPremium: boolean;
 
+  @Column({ nullable: true, type: 'text', select: false })
+  oauthId: string;
+
   @CreateDateColumn()
   createdAt: Date;
 
